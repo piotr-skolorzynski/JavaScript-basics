c95720534912e120e8173d34797461b33762dede
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-27 21:25:22 +0100

    index.html and main.js files modified to show how we use if statement.

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-// booleans
+// if statements
 
-var iLikeMeat = true;
+// let youLikeMeat = true;
+let youLikeMeat = false;
 
-iLikeMeat = false;
+// if (youLikeMeat) {
+//     document.write("Here is the meaty menu..."); 
+// }
 
-myVar = Boolean(7>5); //result is boolean
+// if (7 > 5) {
+//     document.write('this expression is true');
+// }
+
+let myNum = 9;
+
+if (myNum > 10) {
+    document.write('myNum is grater than 10');
+} else {
+    document.write('myNum is lower than 10');
+}
+
+
+if (youLikeMeat) {
+    document.write('You like meat');
+} else {
+    document.write('You hate meat');
+}
