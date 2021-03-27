13d87da4b3f8f574ebd0510bbe0a7f1441d1809f
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-27 18:57:05 +0100

    main.js file modified to show some examples of using basic mathematic operators

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-//example of using camel case
-var myVariableShaun;
+// basic mathematical operators
 
-//setting up variable
-myVariableShaun = 10;
+// = assigment operatator
+var myVar = 5;
 
-var myVar2 = 'hello';
+// + addition
+myVar = myVar + 10;
 
-//changing value
-myVar2 = 'hello again';
+// - substraction 
+myVar = myVar - 10;
 
-//changing the type of variable value
-myVar2 = 5;
+// / - division
+myVar = myVar / 10;
 
-//JS is a weakly type language 
+// * - multiplication
+
+myVar = myVar * 20;
+
+// adding number and string
+var myVar2 = 5 + 'hello'; // converts number to string and concatenate them giving '5hello'. Adding two strings gives the same.
+
+// what happens when you multiply string and number
+
+var myVar3 = 5 * 'hello'; // the result is NaN (not a number)
