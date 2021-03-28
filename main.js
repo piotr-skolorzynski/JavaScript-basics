428dc716ac03ad3fea8e8818dd4aa648e1a81e31
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-28 23:13:05 +0200

    index.html and main.js files modified. Added examples how to change page content and elements atrributes.

--- main.js
+++ main.js
@@ -1,46 +1,30 @@
-// Date object
+//changing page content
 
-let myDate = new Date();
-console.log(myDate);
+//grabbing an element
+const myBody = document.getElementsByTagName('body');
+console.log(myBody);
 
-let myPastDate = new Date(1545, 11, 2, 9, 35, 25) //year, month (from 0 to 11), day, hours, minutes, seconds, 
+// myBody[0].innerHTML = '<p>I am a pargraph tag </p>'; 
 
-let myFutureDate = new Date(2515, 0, 31);
+myLink = document.getElementById('first-link');
+console.log(myLink);
+myLink.textContent = 'This is not a common link';
 
-console.log(myPastDate);
-console.log(myFutureDate);
+// changing element attributes
 
-//some methods
+const testLink = document.getElementById('test');
+console.log(testLink);
 
-let birthday = new Date(1985, 0, 15, 11, 15, 25);
-let birthday2 = new Date(1985, 0, 15, 11, 15, 25);
+const attribute1 = testLink.getAttribute("href");
+console.log(attribute1);
+const attribute2 = testLink.getAttribute('class');
+console.log(attribute2);
 
-//get the mont of the date (0 - 11)
-console.log(birthday.getMonth());
+testLink.setAttribute('class', 'test');
+const attribute3 = testLink.getAttribute('class');
+console.log(attribute3);
 
-//get the full year (YYYY)
-console.log(birthday.getFullYear());
+console.log(testLink.className);
+testLink.className = 'second-link'; //className is a property
+console.log(testLink.className);
 
-//get the date of the month (1 - 31)
-console.log(birthday.getDate());
-
-//get the day of the week (0-6)
-console.log(birthday.getDay());
-
-//get the hour of the date (0-23)
-console.log(birthday.getHours());
-
-//get the number of miliseconds since 1st Jan 1970
-console.log(birthday.getTime());
-
-if (birthday == birthday2){
-    console.log('birtdays are equal');
-} else {
-    console.log('birthdays are not equal');
-} //are not equal because are diffrent objects
-
-if (birthday.getTime() == birthday2.getTime()){
-    console.log('birtdays are equal');
-} else {
-    console.log('birthdays are not equal');
-} //are equal because we are comparing two the same numbers
