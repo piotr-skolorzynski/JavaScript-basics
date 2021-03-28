dfa993b580f88c34bd466bb406afcad26de244c1
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-28 18:13:47 +0200

    main.js file modified to show a little bit about keyword THIS.

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
-// creating objects
+// keyword THIS
 
-let myCar = new Object();
-//adding properties
-myCar.maxSpeed = 50;
-myCar.driver = "Piotr";
-
-console.log(myCar.driver); // Piotr
-
-//adding methods
-
-myCar.drive = function () {
-    console.log('now driving');
-};
-
-//calling method drive on object myCar
-myCar.drive();
-
-//another method to create object
 const myCar2 = {
     maxSpeed: 70,
     driver: "Magda",
     drive: function (speed, time) {
         console.log(`I travelled ${speed * time} km`);
+    },
+    test: function () {
+        console.log(this); 
+    },
+    logDriver: function () {
+        console.log(`driver name is ${this.driver}`);
     }
 };
 
 console.log(myCar2.maxSpeed); //70
 console.log(myCar2.driver); //Magda
 myCar2.drive(30, 2); //I travelled 60 km
+myCar2.test(); //shows object myCar2 - its properties and methods
+myCar2.logDriver();
