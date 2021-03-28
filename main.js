587096c3556cd46012a2d62775b99e0c8719c736
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-28 11:11:51 +0200

    main.js file modified. Added example of function

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-//  loops - practical example
+//  functions
 
-const links = document.getElementsByTagName('a');
+//function declaration
+// function getAverage (a,b) {
+//     let average = (a+b)/2;
+//     console.log(average);
+// }
 
-for (let i = 0; i < links.length; i++) {
-    links[i].className = "link-" + i;
+// //calling function
+// getAverage(7,12);
+
+// if we want function to return value we use command return and assign result to variable
+
+function getAverage (a,b) {
+    let average = (a+b)/2;
+    console.log(average);
+    return average;
 }
+
+let myResult = getAverage(7,8);
+console.log(`the average is ${myResult}`);
