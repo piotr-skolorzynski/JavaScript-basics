4c461dc5a004ecdbb42e4d15d0e26b8d56315527
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-28 18:56:05 +0200

    main.js file modified. Added properties and methods of Date object.

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
-// constructor function
+// Date object
 
-const Car = function (maxspeed, driver) { //constructor function we start with capital letter
-    this.maxspeed = maxspeed;
-    this.driver = driver;
-    this.drive = function (speed, time) {
-        console.log(`I travelled ${speed * time} km`);
-    };
-    this.logDriver = function () {
-        console.log(`The driver name is ${this.driver}`);
-    }
-};
+let myDate = new Date();
+console.log(myDate);
 
-//creating object using constructor
+let myPastDate = new Date(1545, 11, 2, 9, 35, 25) //year, month (from 0 to 11), day, hours, minutes, seconds, 
 
-const myCar = new Car(70, "Piotr");
+let myFutureDate = new Date(2515, 0, 31);
 
-console.log(myCar.maxspeed);
-console.log(myCar.driver);
-myCar.drive(55, 6);
-myCar.logDriver();
+console.log(myPastDate);
+console.log(myFutureDate);
 
-const myCar2 = new Car(120, "James Bond");
-console.log(myCar2.maxspeed);
-console.log(myCar2.driver);
-myCar2.drive(115, 3);
-myCar2.logDriver();
+//some methods
+
+let birthday = new Date(1985, 0, 15, 11, 15, 25);
+let birthday2 = new Date(1985, 0, 15, 11, 15, 25);
+
+//get the mont of the date (0 - 11)
+console.log(birthday.getMonth());
+
+//get the full year (YYYY)
+console.log(birthday.getFullYear());
+
+//get the date of the month (1 - 31)
+console.log(birthday.getDate());
+
+//get the day of the week (0-6)
+console.log(birthday.getDay());
+
+//get the hour of the date (0-23)
+console.log(birthday.getHours());
+
+//get the number of miliseconds since 1st Jan 1970
+console.log(birthday.getTime());
+
+if (birthday == birthday2){
+    console.log('birtdays are equal');
+} else {
+    console.log('birthdays are not equal');
+} //are not equal because are diffrent objects
+
+if (birthday.getTime() == birthday2.getTime()){
+    console.log('birtdays are equal');
+} else {
+    console.log('birthdays are not equal');
+} //are equal because we are comparing two the same numbers
