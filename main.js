378b5d2a3fe7e16be9962c68ee66e00979b38e97
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-28 23:26:22 +0200

    index.html and main.js files modified to show how using js we can change css styles.

--- main.js
+++ main.js
@@ -1,30 +1,14 @@
-//changing page content
+//changing css styles
 
-//grabbing an element
-const myBody = document.getElementsByTagName('body');
-console.log(myBody);
+const title = document.getElementById("page-title");
+console.log(title);
 
-// myBody[0].innerHTML = '<p>I am a pargraph tag </p>'; 
+title.setAttribute('style', 'position: relative;');
 
-myLink = document.getElementById('first-link');
-console.log(myLink);
-myLink.textContent = 'This is not a common link';
+title.setAttribute('style', 'position: relative; left: 10px;');
 
-// changing element attributes
-
-const testLink = document.getElementById('test');
-console.log(testLink);
-
-const attribute1 = testLink.getAttribute("href");
-console.log(attribute1);
-const attribute2 = testLink.getAttribute('class');
-console.log(attribute2);
-
-testLink.setAttribute('class', 'test');
-const attribute3 = testLink.getAttribute('class');
-console.log(attribute3);
-
-console.log(testLink.className);
-testLink.className = 'second-link'; //className is a property
-console.log(testLink.className);
+title.style.left = "200px";
+title.style.top = "100px";
+title.style.color = "red";
+title.style.backgroundColor = "blue";
 
