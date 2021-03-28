734e9a038119b0f8f689317e841141f1c1f76b4f
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-28 13:01:04 +0200

    main.js file modified. Added info about arrays

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-// slice and split strings
+// arrays
 
-let str = 'hello, world';
+//creating array
+let myArray = [];
 
-let str2 = str.slice(2);
-console.log(str2); // llo, world
+myArray[0] = 25;
+console.log(myArray[0]); // 25
 
-str3 = str.slice(2,9);
-console.log(str3); // llo, wo
+//adding values
+myArray[1] = 35;
 
-//split
-let tags = 'meat, ham, salami, pork, beef, chicken';
-let tagsArray = tags.split(',');
-console.log(tagsArray);
+myArray[2] = true;
 
-// visit MDN site to see more methods
+myArray[3] = 'hello';
+
+console.log(myArray); // [25, 35, true, "hello"]
+
+// updating values
+myArray[2] = false;
+console.log(myArray); // [25, 35, false, "hello"]
+
+//another array creation
+let myArray2 = [10,20,"hi",false];
+
+//other method
+let myArray3 = new Array();
+
+let myArray4 = new Array(5); //creats new Array with 5 epmty slots 
+
+//properties and methods
+
+console.log(myArray2.length); // 4 
+
+console.log(myArray2.sort()); // [10, 20, false, "hi"]
+console.log(myArray2.reverse()); // ["hi", false, 20, 10]
+
+// to see more visit MDN site
