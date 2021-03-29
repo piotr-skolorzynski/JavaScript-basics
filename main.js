b35f78481baf3ab923332655cc8193475996528b
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-03-29 17:42:28 +0200

    index.html and main.js files modified. Added example of using window.onload event

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-// onclick event
+function setUpEvents () {
+    //grabbing elements
+    const content = document.getElementById('content');
+    const button = document.getElementById('show-more');
+    // onclick event
+    button.onclick = function () {
+        if (content.className == "open") {
+            //shrink the box
+            content.className = "";
+            button.textContent = "show more";
+        } else {
+            //expand the box
+            content.className = "open";
+            button.textContent = "Show less";
+        }
+    };
+};
 
-//grabbing elements
-
-const content = document.getElementById('content');
-const button = document.getElementById('show-more');
-
-button.onclick = function () {
-    if (content.className == "open") {
-        //shrink the box
-        content.className = "";
-        button.textContent = "show more";
-    } else {
-        //expand the box
-        content.className = "open";
-        button.textContent = "Show less";
-    }
+//onload event
+window.onload = function () {
+    setUpEvents();
 };
